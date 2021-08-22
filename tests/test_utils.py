2874e2eb0d65e134cd5e6b455f4d776775c18dd0
Alexander Terbeznik <alexander.terbeznik@gmail.com>
2021-08-22 15:42:09 +0200

    add synth_ts function with tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,5 @@
 import zeug
+import numpy as np
 
 
 def test_sin_cos_transformation():
@@ -8,3 +9,15 @@ def test_sin_cos_transformation():
     a_sin, a_cos = zeug.sin_cos_transformation(a, period=period)
 
     assert a_sin.shape == a_cos.shape
+
+
+def test_synth_ts():
+    ts1 = zeug.synth_ts()
+    ts2 = zeug.synth_ts(n=1000)
+    ts3 = zeug.synth_ts(base=50, r_down=0, r_up=0)
+    ts4 = zeug.synth_ts(base=30, r_down=0)
+
+    assert type(ts1) == np.ndarray
+    assert len(ts2) == 1000
+    assert ts3[-1] == 50
+    assert ts4[-1] > 30
--- zeug/utils.py
+++ zeug/utils.py
@@ -11,3 +11,14 @@ def sin_cos_transformation(a, period=None):
     a_cos = np.cos((2*np.pi*a)/period)
 
     return a_sin, a_cos
+
+
+def synth_ts(n=100, base=100, r_down=0.05, r_up=0.05):
+    ts = np.array([])
+
+    for t in range(n):
+        r = np.random.uniform(low=1-r_down, high=1+r_up)
+        base = base*r
+        ts = np.append(ts, base)
+
+    return ts
